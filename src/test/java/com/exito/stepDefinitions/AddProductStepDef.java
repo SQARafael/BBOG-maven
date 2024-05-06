c2ba5dff6baf49fd9b7c2382632ec7544630a464
Rafael Chica <rafael.chica@sqasa.co>
2024-05-06 12:18:43 -0500

    repo exito, plantilla inicial

---
+++ src/test/java/com/exito/stepDefinitions/AddProductStepDef.java
@@ -0,0 +1,38 @@
+package com.exito.stepDefinitions;
+/*
+ * @(#) AddProductStepDef.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+import io.cucumber.java.en.*;
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class AddProductStepDef {
+    @Given("que el usuario ingresa a la pagina web")
+    public void queElUsuarioIngresaALaPaginaWeb() {
+
+    }
+    @When("el usuario ingresa al menu")
+    public void elUsuarioIngresaAlMenu() {
+
+    }
+    @When("Buca la categoria y subcategoria")
+    public void bucaLaCategoriaYSubcategoria() {
+
+    }
+    @When("Selecciona el producto")
+    public void seleccionaElProducto() {
+
+    }
+    @When("Dar click en el boton de agregar al carrito")
+    public void darClickEnElBotonDeAgregarAlCarrito() {
+
+    }
+    @Then("Se podra visualizar el producto en el carrito de compras")
+    public void sePodraVisualizarElProductoEnElCarritoDeCompras() {
+
+    }
+}
